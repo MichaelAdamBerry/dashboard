04701b40e71f6bdecd2e26d6f950bcf629237963
MIchael Berry <michaeladamberry3@gmail.com>
2019-07-07 13:01:31 -0400

    migrated code from codepen and set up express server

---
+++ charts/p5-canvas-map.js
@@ -0,0 +1,83 @@
+//p5 canvas functions
+let mapImg;
+let cLon = -94.8492;
+let cLat = 37.6767;
+let w = 500;
+let h = 400;
+let zoom = 2.06;
+
+const cities = [
+  {
+    city: "Austin",
+    state: "TX"
+  },
+  {
+    city: "San Francisco",
+    state: "CA"
+  },
+  {
+    city: "Montgomery",
+    state: "AL"
+  },
+  {
+    city: "San Diego",
+    state: "CA"
+  },
+  {
+    city: "Helena",
+    state: "MT"
+  },
+  {
+    city: "Raleigh",
+    state: "NC"
+  },
+  {
+    city: "Omaha",
+    state: "NE"
+  },
+  {
+    city: "New York",
+    state: "NY"
+  }
+];
+
+async function preload() {
+  mapImg = await loadImage(
+    "https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/-95.6632,36.5979,2.55,2,5/500x400?access_token=pk.eyJ1IjoibWljaGFlbGFkYW1iZXJyeTMiLCJhIjoiY2p4c2Vuc3kxMGkyNDNmbWpkYTMwbWR3dSJ9.3AsBmc5Rpw4xcj4_EPF5bA"
+  );
+}
+
+function mercX(lon) {
+  lon = radians(lon);
+  var a = (256 / PI) * pow(2, zoom);
+  var b = lon + PI;
+  return a * b;
+}
+
+function mercY(lat) {
+  lat = radians(lat);
+  var a = (256 / PI) * pow(2, zoom);
+  var b = tan(PI / 4 + lat / 2);
+  var c = PI - log(b);
+  return a * c;
+}
+
+function setup() {
+  let ctx = createCanvas(w, h);
+  ctx.parent("us-map");
+  //translate(width / 2, height / 2);
+  //imageMode(CENTER);
+  image(mapImg, 0, 0);
+
+  var cx = mercX(cLon);
+  var cy = mercY(cLat);
+}
+
+function randomDate(type) {
+  //type should be "start" or "end"
+  let month, day, year;
+  year = type === "start" ? "19" : "20";
+  month = Math.floor(Math.random() * 12);
+  day = Math.floor(Math.random() * 30);
+  return `${month}/${day}/${year}`;
+}
